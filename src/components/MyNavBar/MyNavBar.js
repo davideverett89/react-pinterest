afede9f7fae1d6570ab19946624de5e95c4813d4
David Everett <davideverett1989@gmail.com>
2020-05-18 18:32:52 -0500

    Inital setup components in place.

---
+++ src/components/MyNavBar/MyNavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MyNavBar.scss';
+
+class MyNavBar extends React.Component {
+  render() {
+    return (
+          <div className="MyNavBar">
+              <h1>NAVBAR COMPONENT</h1>
+          </div>
+    );
+  }
+}
+
+export default MyNavBar;
