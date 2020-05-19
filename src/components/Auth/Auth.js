3ea7367536fdfcf10d3c6b86bf9a706f10bcba2d
David Everett <davideverett1989@gmail.com>
2020-05-18 19:11:39 -0500

    Login works.

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Auth.scss';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
   render() {
     return (
           <div className="Auth">
-              <h1>AUTH COMPONENT</h1>
+              <button className="btn btn-primary" onClick={this.loginClickEvent}>Google Auth</button>
           </div>
     );
   }
