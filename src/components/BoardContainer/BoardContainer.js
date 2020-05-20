a1327ae5d65ac19ab4d8db51b8edecc252bae845
David Everett <davideverett1989@gmail.com>
2020-05-19 19:16:05 -0500

    Successfully reading boards from firebase.

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './BoardContainer.scss';
 
+import boardsData from '../../helpers/data/boardsData';
+import authData from '../../helpers/data/authData';
+
+import Board from '../Board/Board';
+
 class BoardContainer extends React.Component {
+  state ={
+    boards: [],
+  }
+
+  componentDidMount() {
+    boardsData.getBoardsByUid(authData.getUid())
+      .then((boards) => this.setState({ boards }))
+      .catch((err) => console.error('Unable to get all boards:', err));
+  }
+
   render() {
+    const { boards } = this.state;
+    const makeBoards = boards.map((board) => (
+      <Board key={board.id} board={board} />
+    ));
+
     return (
           <div className="BoardContainer">
-              <h1>BOARD CONTAINER COMPONENT</h1>
+              <h2>Boards</h2>
+              <div className="d-flex flex-wrap">
+                {makeBoards}
+              </div>
           </div>
     );
   }
