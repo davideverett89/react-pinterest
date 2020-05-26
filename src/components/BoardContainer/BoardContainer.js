5fdb6e433c8dd01b7372b527e723c99c91354792
David Everett <davideverett1989@gmail.com>
2020-05-26 18:20:04 -0500

    Board form component created and linked.

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -7,6 +7,7 @@ import authData from '../../helpers/data/authData';
 import smash from '../../helpers/data/smash';
 
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 class BoardContainer extends React.Component {
   static propTypes = {
@@ -15,6 +16,7 @@ class BoardContainer extends React.Component {
 
   state ={
     boards: [],
+    formOpen: false,
   }
 
   getInfo = () => {
@@ -34,7 +36,7 @@ class BoardContainer extends React.Component {
   }
 
   render() {
-    const { boards } = this.state;
+    const { boards, formOpen } = this.state;
     const { setSingleBoard } = this.props;
     const makeBoards = boards.map((board) => (
       <Board key={board.id} board={board} setSingleBoard={setSingleBoard} removeBoard={this.removeBoard} />
@@ -43,6 +45,8 @@ class BoardContainer extends React.Component {
     return (
           <div className="BoardContainer">
               <h2>Boards</h2>
+              <button className="m-3 btn btn-success" onClick={() => this.setState({ formOpen: true })}><i className="fas fa-plus"></i></button>
+              { formOpen ? <BoardForm /> : ''}
               <div className="d-flex flex-wrap">
                 {makeBoards}
               </div>
---
+++ src/components/BoardForm/BoardForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './BoardForm.scss';
+
+class BoardForm extends React.Component {
+  render() {
+    return (
+        <div className="BoardForm">
+            <h2>This is the form.</h2>
+        </div>
+    );
+  }
+}
+
+export default BoardForm;
